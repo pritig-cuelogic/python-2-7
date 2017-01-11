2bb510df21b9ddfde87506986ac71e71f60f0eb0
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-11 18:01:36 +0530

    ex4

---
+++ ex4.py
@@ -0,0 +1,18 @@
+print 13.4
+print 5 / 2
+print 3 * 1.2
+
+cars = 100
+space_in_car = 4.0
+drivers = 25
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_car
+average_passengers_per_car = passengers /  cars_driven
+drivers = 45
+
+print "there are",cars,"cars available"
+print "There are only",drivers,"drivers available"
+print "We have",passengers,"passengers only"
+
