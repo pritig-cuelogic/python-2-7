6642da73e75d520794b0a1cb642689100491adc3
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-11 18:33:03 +0530

    ex2

--- ex2.py
+++ ex2.py
@@ -1,5 +1,7 @@
 # print "reiya"
 print "neha #" "rehana"
 print "Hello"# rest part will be ignore
-
-print"hi"
+print "hi"
+# print "multiline comment1"
+# print "multiline comment2"
+# print "multiline comment3"
