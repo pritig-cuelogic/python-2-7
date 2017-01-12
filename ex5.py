13235663de2cf7bd675dcebbf6754134d44a813e
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-12 11:38:02 +0530

    changes

--- ex5.py
+++ ex5.py
@@ -11,5 +11,6 @@ print "my age is %d" % my_age
 print "he is %d pound heavy" % my_weight
 print "i have %r hair with my own"% my_hair
 print "he has %s eyes and %s hair" % (my_eyes, my_hair)
-print "if i add %d, %d, %d then i will get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "if i add %d, %d, %d then i will get %d" % (
+	my_age, my_height, my_weight, my_age + my_height + my_weight)
 print "My Name is %s and my age is %d" % (name, my_age)
