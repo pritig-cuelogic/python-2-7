165a117742bad420e44682c60a5b97d0380750d1
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-12 12:19:03 +0530

    ex6

---
+++ ex6.py
@@ -0,0 +1,20 @@
+x = "There are %d schools in %s city" % (10, "Agra")
+print x
+y = "Boolean"
+print y
+r = "Rehan"
+z = "%s calculated %s values" % (r, y)
+print z
+z = "%r calculated %r values" % (r, y)
+print z
+k = "do't\n"
+check_k = "you %s" % k
+print check_k
+check_k = "you %r" % k
+print check_k
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+print joke_evaluation % hilarious
+joke_evaluation = "Isn't that joke so funny?! %s"
+print joke_evaluation % hilarious
+print "this is right" + " " + "that is right"
