589b2ea006229e3645b48ca548d82f1a8860ee5a
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-12 16:12:25 +0530

    ex10

---
+++ ex10.py
@@ -0,0 +1,20 @@
+print 'i "understood" problem\b'
+print "\fi \"understood\' problem"
+print "I am 6'2\" tall."
+print "I am 6\'2\" tall."
+tabby_cat = "\tI'm tabbed in."
+print tabby_cat
+persian_cat = "I'm split\non a line.\b"
+print persian_cat
+backslash_cat = "I'm \\ a \\ cat."
+print backslash_cat
+print "I'm \a \\ cat."
+print """
+      I'll do a list:
+      \t* Cat food
+      \t* Fishies
+      \t* Catnip\n\t* Grass
+      """
+print "she is \rg"
+print "let's work\f with u"
+print "let's work\v with u"
