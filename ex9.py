2b3d88d6e06e35af949b7d32412b7a5df0117428
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-12 15:00:37 +0530

    ex9

---
+++ ex9.py
@@ -0,0 +1,8 @@
+days = "mon tue wed thu fri sat sun"
+print "these are days: ", days
+month = "jan\nfeb\nmarch\napr\nmay"
+print "these are month: ", month
+print """
+      this is very.
+      There's something going on here.
+      We'll be able to type as much as we like. """
