8f354b7dd0f81794399129a89c97c453664500d4
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-13 16:07:24 +0530

    ex21

---
+++ ex21.py
@@ -0,0 +1,22 @@
+def add_number(a,b):
+	print "sum = %d" % (a+b)
+	return (a+b)
+
+def subtract_number(a,b):
+	print "subtraction = %d" % (a-b)
+	return (a-b)
+
+def multiply_number(a,b):
+	print "multiply = %d" % (a*b)
+	return (a*b)
+
+def devide_number(a,b):
+	print "Devide = %d" % (a/b)
+	return (a/b)
+
+print add_number(4, 5)
+print subtract_number(10, 2)
+print multiply_number(2, 4)
+print devide_number(10, 2)
+
+
