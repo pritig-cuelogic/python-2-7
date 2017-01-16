4aeffe79f598a15ec0a6a9af11646a3cc1d0538f
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-16 17:59:51 +0530

    ex34

---
+++ ex34.py
@@ -0,0 +1,6 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at 1: %s" % animals[0]
+print "The third (3rd) animal is %s" % animals[2]
+print "The animal at 2 is %s" % animals[1]
+print "The animal at 4 is %s" % animals[3]
