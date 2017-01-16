b5028c9ab3bcd83981020d35c0195d1e333cee81
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-16 12:41:24 +0530

    ex28

---
+++ ex28.py
@@ -0,0 +1,24 @@
+print 3 != 4 and not ("testing" != "test" or "Python" == "Python")
+print True and not ("testing" != "test" 
+	or "Python" == "Python")
+print True or True
+print not (True)
+print True and False
+print True and False
+print True and True
+print False and True
+print 1 == 1
+print 1 == 1 and 2 == 1
+print "test" == "test"
+print "test" == "testing"
+print 1 != 0 and 2 == 1
+print not (1 == 1 and 0 != 1)
+print 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+print 1 and 1
+print "test" or "test"
+print False and 1
+print False or 1
+print 1 and True
+print True and 1
+print 0 or False
+
