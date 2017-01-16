abf9e997ee2b41e089a5d2df85fbe84cbea098eb
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-16 14:23:58 +0530

    ex29

---
+++ ex29.py
@@ -0,0 +1,34 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats."
+
+if people > cats:
+	print "Too many people."
+
+if people < dogs:
+	print "Too many dogs."
+
+if people > dogs:
+	print "too many people then dogs."
+
+dogs += 5
+
+if people < dogs:
+	print "Too many dogs."
+
+if people > dogs:
+	print "too many people then dogs."
+
+if people == dogs:
+	print "both are equal"
+
+if 1 and 1:
+	print 1 and 1
+	print "this is starting of if block"
+	print dogs
+	x = 90
+
+print x
