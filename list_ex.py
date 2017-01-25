8e162fcf67beaae68f331164d36e1af5bcb5c952
Priti Gupta <priti.gupta@cuelogic.co.in>
2017-01-25 14:34:09 +0530

    python practice ex

---
+++ list_ex.py
@@ -0,0 +1,32 @@
+a = [1, 2, 3, 4, 9, 11]
+b = [5, 6]
+
+print a + b
+print a * 3
+
+print a[1:4]
+print a[1:-2]
+
+x = range(12)
+print x
+print x[1:11:2]
+print x[::-1]
+print 4 in x
+
+print zip(["neetu", 1, [1,2]], [[4,5], "riya", 67, 8, 9])
+
+for key, val in zip(["neetu", 1, [1,2]], [[4,5], "riya", 67, 8, 9]):
+	print key, val
+
+print sum(a)
+
+a = [2, 10, 4, 3, 7]
+a.sort()
+print a
+
+b = [4, 3, 5, 9, 2]
+print sorted(b)
+print b
+
+print [x+y for x,y in zip([2, 3, 5, 88], [6, 8, 90])]
+print [x+y for x in [4,5,6] for y in [1, 2, 3] if (x+y) < 7 and (x+y) != 6]
